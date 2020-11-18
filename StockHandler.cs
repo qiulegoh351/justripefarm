97e9c62245c290f8c4226291c6119c1e196c9fe2
cheewengng@gmail.com <cheewengng@gmail.com>
2020-11-18 11:52:41 +0800

    Add logger and singleton in stockhandler

--- StockHandler.cs
+++ StockHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,30 @@ namespace GUI___Multi_From_and_Panel
 {
     public class StockHandler
     {
+        private String logStFile = "Stock_log.txt";
+        private StreamWriter writer;
+
+        private static StockHandler SH_instance;
+
+        public static StockHandler sh_instance
+        {
+            get
+            {
+                if (SH_instance == null)
+                    SH_instance = new StockHandler();
+                return SH_instance;
+            }
+        }
+
+        private StockHandler()
+        {
+            try
+            {
+                writer = new StreamWriter(logStFile, true);
+            }
+            catch (IOException e) { }
+        }
+
         public int addNewStock(MySqlConnection conn, Stock stock)
         {
             string sql = "INSERT INTO stock (type, quantity, supplier_id, purchase_date, expiry_date) "
@@ -17,7 +42,21 @@ namespace GUI___Multi_From_and_Panel
                         + "   ', '" + stock.ExpiryDate.ToString("yyyy-MM-dd HH:mm:ss") + "')";
 
             MySqlCommand sqlComm = new MySqlCommand(sql, conn);
+
+            writer.WriteLine(" Succcessful Add New Labourer to Database!" + '\n' +
+            " Type: " + stock.Type + '\n' + " Quantity: " + stock.Quantity + '\n' + " Supplier ID: " + stock.SupplierID + '\n' + " Purchase Date: " + stock.PurchaseDate + " /nPurchase Date: " + ExpiryDate);
             return sqlComm.ExecuteNonQuery();
+        }
+
+        public void Close()
+        {
+            writer.Close();
+        }
+
+        public void Open()
+        {
+            writer = new StreamWriter(logStFile, true);
+            writer.WriteLine();
         }
     }
 }
